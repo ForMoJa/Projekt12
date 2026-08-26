3c63e377a3ef62e653d62bbfdf4aa7c0d204bdd5
ForMoJa <moritzjahn@t-online.de>
2026-08-26 09:26:54 +0200

    bug fix

--- DE_test.py
+++ DE_test.py
@@ -5,31 +5,6 @@ from functools import reduce
 from fractions import Fraction
 from operator import getitem
 
-# def get_deg_models(n):
-#     A = [[0]*n]
-#     while len(A) != 0:
-#         c = A.pop()
-#         ma = 0
-#         o = []
-#         for i in range(0, n):
-#             ma = max(ma, c[i])
-#             if c[i] == 0:
-#                 o.append(i)
-#         m = len(o)
-#         for i in range(1,(1 << m)-1):
-#             P = []
-#             for j in range(m):
-#                 if(i & (1<<j)):
-#                     P.append(o[j])
-#             d = copy.deepcopy(c)
-#             for j in range(0, len(P)):
-#                 d[P[j]] = ma+1
-#             A.append(d)
-#         d = copy.deepcopy(c)
-#         for k in o:
-#             d[k] = ma+1
-#         yield d
-
 def complement_p_supp(S, p):
     '''
     S is a set of strategies 
@@ -79,59 +54,6 @@ def calculate_expectency(Xi, S, p, i, si=None):
             expected += reduce(getitem, s, Xi) * reduce(getitem, s, p) # type: ignore
     return expected     
 
-# def solve_inequalites(X, m, maxD, E, vanishing_marginals, verbose=False):
-#     '''
-# 	X pay-off matrices
-#     m is given model
-# 	maxD maximal degree of the model
-# 	E is a list where E[i] is the expected pay-off of player i
-#     '''
-#     n = len(vanishing_marginals)
-
-#     if verbose:
-#         print(f"Testing model m={m}")
-
-#     min_deg = [maxD]*len(vanishing_marginals)
-#     # for i in range(vanishing_marginals):
-#         # min_deg[i]=maxD
-
-#     for j in range(len(vanishing_marginals)):
-#         si = vanishing_marginals[j]
-#         for s in m.keys():
-#             if s[si[0]] == si[1]:
-#                 min_deg[j] = min(min_deg[j], m[s])
-    
-#     for d in range(1, maxD + 1): # solving a LP for each of the degrees
-#         J = []
-#         global count_polytopes
-#         count_polytopes+=1
-#         for s in m.keys():
-#             if m[s] == d:
-#                 J.append(s)
-#         if verbose:
-#             print(f"d={d}, J ={J}")
-
-#         # weight vector is simply zeros; 
-#         # optimzing constant function is equivlanet to checking whether feasible set is empty
-#         c = np.zeros(len(J)) 
-#         b = np.zeros(n)
-
-#         A = np.zeros((n, len(J)))
-#         for k, si in enumerate(vanishing_marginals):
-#             for l, s in enumerate(J):
-#                 if si[1] == s[si[0]] and min_deg[k] == d:
-#                     A[k][l] = reduce(getitem, s, X[si[0]]) - E[si[0]] # type: ignore
-
-#         LP_res = scipy.optimize.linprog(c, A_ub = A, b_ub = b, bounds = (1, None))
-#         status = LP_res.status 
-#         x = LP_res.x
-#         if status != 0:
-#             return False
-#         if verbose:
-#             print(f"Found solution at d={d}, namely x={x}")
-
-#     return True
-
 def get_S(D):
     '''
     D is a tuple of integers
@@ -148,67 +70,6 @@ def get_S(D):
         for k in range(d):
             H.append([k]+i)
     return H
-
-# def test_DE(X, p, D):
-#     '''
-#     X is a game
-#     p is a probability distribution
-#     D is a tuple of pure strategies of the player
-
-#     return whether p is DE in game X
-#     '''
-
-#     E = copy.deepcopy(D)
-#     S = get_S(E)
-#     print("strategies: ", S)
-#     print("number of pure strategies for each player: ", D)
-
-#     expected_val = []
-#     for i in range(0, len(D)):
-#         expected_val.append(calculate_expectency(X[i], S, p, i))
-        
-#     print("Expected value for each player: ", expected_val)
-#     marginals = []
-#     vanishing_marginals = []
-#     for i in range(0, len(D)):
-#         for j in range(0, D[i]):
-#             if(p_si(S, p, i, j) > 0):
-#                 marginals.append([i, j])
-#             else:
-#                 vanishing_marginals.append([i, j])
-#     print("Marginals: ")
-#     print(marginals)
-#     print("\nVanishing Marginals: ")
-#     print(vanishing_marginals)
-
-#     # check whether p lies on the Spohn variety; if not, p cannot possible be a DE    
-#     for si in marginals:
-#         if expected_val[si[0]] != calculate_expectency(X[si[0]], S, p, si[0], si[1]):
-#             print("p is not on the Spohn variety!")
-#             print("p is not DE by s_i=", si)
-#             print(expected_val[si[0]],"!=",calculate_expectency(X[si[0]], S, p, si[0], si[1]))
-#             return False
-        
-#     T = complement_p_supp(S, p)
-#     for j in range(len(T)):
-#         T[j]=tuple(T[j])
-#     # if all marginals of p are defined, lying on the Spohn variety is already a sufficient condition -> return True
-#     if len(vanishing_marginals) == 0:
-#         print("Lies on the Spohn ideal and all marginals defined.")
-#         return True
-
-#     dic = {}
-#     for m in get_deg_models(len(T)):
-#         # print(m)
-#         maxd = 0
-#         for i in range(0, len(T)):
-#             dic[T[i]] = m[i]
-#             maxd = max(maxd, m[i])
-#         # print(dic)
-#         if solve_inequalites(X, dic, maxd, expected_val, vanishing_marginals):
-#             print("Model found: ", m)
-#             return True
-#     return False
 	
 def bach_strawinsky():
     '''
@@ -367,7 +228,8 @@ def test_DE(X, p, D, verbose=False):
 					marginals_resolved.append(v)
 				else:
 					vm_left.append(v)
-			
+			if len(marginals_resolved) == 0:
+				continue
 			global count_polytopes
 			count_polytopes += 1
 			# Check polytope
@@ -429,8 +291,6 @@ if __name__ == "__main__":
 
     S = get_S([3,3])
     print(f"S = {S}")
-
-    # print(calculate_expectency(X[0], S, p, 0))
 
     if test_DE(X, p, [3,3]):
         print(f"=============")
